a1cfd94454545c3d78808c56793f0dddbb653279
Sharif <kopisharif@gmail.com>
2026-07-24 20:15:15 +0700

    add any changes

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using API.Entities;
 using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,6 +10,22 @@ namespace API.Controllers
     [ApiController]
     public class MembersController(AppDbContext context) : ControllerBase
     {
-     
+
+     [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+            return members;
+        }
+    [HttpGet("{id}")]//localhost:5000/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+            if (member == null) 
+            {
+                return NotFound();
+            }
+            return member;
+        }
     }
 }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31ac6850eb346783a3678a6f4e3e7d518590eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63f2bf3ce59e887d40119d89a82305f7d80595f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
