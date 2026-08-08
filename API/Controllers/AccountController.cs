6fb49caef6f36fab9c8025cc2b0508ea1ca78dd4
Sharif <kopisharif@gmail.com>
2026-08-08 22:32:57 +0700

    Creating UserDto and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,14 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
 using API.DTOs;
 
 namespace API.Controllers
 {
-    public class AccountController(AppDbContext context) : BaseApiController
+    public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
     {
        [HttpPost("register")]
-       public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+       public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
        {
         if(await EmailExists(registerDto.Email)) return BadRequest("Email is already taken");
 
@@ -28,11 +29,17 @@ namespace API.Controllers
             context.Users.Add(user);
             await context.SaveChangesAsync();
 
-            return user;
-       }
+            return new UserDto
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = tokenService.CreateToken(user)
+            };
+    }
 
        [HttpPost("login")]
-       public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+       public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
          {
             var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
 
@@ -49,7 +56,13 @@ namespace API.Controllers
                     return Unauthorized("Invalid password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = tokenService.CreateToken(user)
+            };
          }
 
        private async Task<bool> EmailExists(string email)
