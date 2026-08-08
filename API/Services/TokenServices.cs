4ad32cb2da2fe04a04af30aad7f668d834735958
Sharif <kopisharif@gmail.com>
2026-08-08 22:18:16 +0700

    Create A token Services

--- API/Services/TokenServices.cs
+++ API/Services/TokenServices.cs
@@ -1,13 +1,43 @@
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace API.Services
 {
-    public class TokenService : ITokenService
+    public class TokenService(IConfiguration config) : ITokenService
     {
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var tokenkey = config["TokenKey"] ?? throw new Exception("Cannot get TokenKey");
+
+            if (tokenkey.Length < 64)
+                throw new Exception("Your TokenKey needs to be >= 64 characters");
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenkey));
+
+                var claims = new List<Claim>
+                {
+                    new (ClaimTypes.Email, user.Email),
+                    new (ClaimTypes.NameIdentifier, user.Id)
+                };
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(7),
+                    SigningCredentials = creds
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return tokenHandler.WriteToken(token);
+            
         }
     }
 }
